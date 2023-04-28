a0c6da2e8f29f9e5570700b77518d5500b398a09
ZZBAE <jebyun2@gmail.com>
2023-04-28 20:31:54 +0900

    feat: API연결

--- NetflixInstagram/Controllers/HomeViewController.swift
+++ NetflixInstagram/Controllers/HomeViewController.swift
@@ -7,6 +7,14 @@
 
 import UIKit
 
+enum Sections: Int {
+    case TrendingMovies = 0
+    case TrendingTv = 1
+    case Popular = 2
+    case UpComing = 3
+    case TopRated = 4
+}
+
 class HomeViewController: UIViewController {
 
     let sectionTitles: [String] = ["Trending Movies", "Trending TV", "Popular", "Upcoming Movies", "Top rated"]
@@ -30,7 +38,6 @@ class HomeViewController: UIViewController {
         homeFeedTable.tableHeaderView = headerView
 
         configureNavBar()
-        fetchData()
     }
 
     private func configureNavBar() {
@@ -63,21 +70,6 @@ class HomeViewController: UIViewController {
         super.viewDidLayoutSubviews()
         homeFeedTable.frame = view.bounds
     }
-
-    private func fetchData() {
-//        APICaller.shared.getTrendingMovies { results in
-//            switch results {
-//            case .success(let movies):
-//                print(movies)
-//            case .failure(let error):
-//                print(error)
-//            }
-//        }
-
-        APICaller.shared.getTrendingTVs { results in
-
-        }
-    }
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
@@ -92,6 +84,60 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+            return UITableViewCell()
+        }
+
+        switch indexPath.section {
+        case Sections.TrendingMovies.rawValue:
+            APICaller.shared.getTrendingMovies { result in
+                switch result {
+                case .success(let titles):
+                    cell.configure(with: titles)
+                case .failure(let error):
+                    print(error.localizedDescription )
+                }
+            }
+        case Sections.TrendingTv.rawValue:
+            APICaller.shared.getTrendingTVs { result in
+                switch result {
+                case .success(let titles):
+                    cell.configure(with: titles)
+                case .failure(let error):
+                    print(error.localizedDescription )
+                }
+            }
+
+        case Sections.Popular.rawValue:
+            APICaller.shared.getPopular { result in
+                switch result {
+                case .success(let titles):
+                    cell.configure(with: titles)
+                case .failure(let error):
+                    print(error.localizedDescription )
+                }
+            }
+
+        case Sections.UpComing.rawValue:
+            APICaller.shared.getUpcomingMovies { result in
+                switch result {
+                case .success(let titles):
+                    cell.configure(with: titles)
+                case .failure(let error):
+                    print(error.localizedDescription )
+                }
+            }
+
+        case Sections.TopRated.rawValue:
+            APICaller.shared.getTopRated { result in
+                switch result {
+                case .success(let titles):
+                    cell.configure(with: titles)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+
+        default:
             return UITableViewCell()
         }
 
--- NetflixInstagram/Models/Title.swift
+++ NetflixInstagram/Models/Title.swift
@@ -1,17 +1,17 @@
 //
-//  Tv.swift
-//  NetflixInstagram
+//  Movie.swift
+//  Netflix+Instagram
 //
 //  Created by 변재은 on 2023/04/24.
 //
 
 import Foundation
 
-struct TrendingTvResponse: Codable {
-    let results: [Tv]
+struct TrendingTitleResponse: Codable {
+    let results: [Title]
 }
 
-struct Tv: Codable {
+struct Title: Codable {
     let id: Int
     let media_type: String?
     let original_name: String?
