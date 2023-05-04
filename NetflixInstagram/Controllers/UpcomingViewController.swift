7514e60e431ae194ed8e0966d087edb45946cbcf
ZZBAE <jebyun2@gmail.com>
2023-05-04 16:53:05 +0900

    feat: 성모병원^^

--- NetflixInstagram/Controllers/UpcomingViewController.swift
+++ NetflixInstagram/Controllers/UpcomingViewController.swift
@@ -9,14 +9,14 @@ import UIKit
 
 class UpcomingViewController: UIViewController {
     private lazy var collectionView: UICollectionView = {
-        let view = UICollectionView(frame: .zero, collectionViewLayout: self.compositionalLayout)
+        let view = UICollectionView(frame: .zero, collectionViewLayout: self.createLayout())
         view.isScrollEnabled = true
         view.showsHorizontalScrollIndicator = true
         view.showsVerticalScrollIndicator = false
         view.showsHorizontalScrollIndicator = false
-        view.contentInset = .zero
-        view.clipsToBounds = true
-        view.register(InstaStoryCell.self, forCellWithReuseIdentifier: "InstaStoryCell")
+//        view.contentInset = .zero
+//        view.clipsToBounds = true
+//        view.register(InstaStoryCell.self, forCellWithReuseIdentifier: "InstaStoryCell")
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
@@ -25,7 +25,15 @@ class UpcomingViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
 
+        setupCollectionView()
         configureNavBar()
+    }
+
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.collectionViewLayout = createLayout()
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     }
 
     private func configureNavBar() {
@@ -47,5 +55,95 @@ class UpcomingViewController: UIViewController {
         ]
         navigationController?.navigationBar.tintColor = .white
     }
+
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { section, env -> NSCollectionLayoutSection? in
+            let item = NSCollectionLayoutItem(
+              layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1/5),
+                heightDimension: .fractionalHeight(1)
+              )
+            )
+            let group = NSCollectionLayoutGroup.horizontal(
+              layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(0.2),
+                heightDimension: .absolute(140)
+              ),
+              subitems: [item])
+            group.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 0, bottom: 16, trailing: 0)
+
+            // section
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .continuous
+            section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20)
+
+            // return
+            return section
+
+          }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
 }
 
+extension UpcomingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+
+        cell.backgroundColor = .red
+        cell.layer.cornerRadius = 8
+
+        return cell
+    }
+}
+
+//extension UpcomingViewController: UICollectionViewDelegateFlowLayout {
+//  private func createCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+//    return UICollectionViewCompositionalLayout { (section, _) -> NSCollectionLayoutSection? in
+//      if section == 0 {
+//        // item
+//        let item = NSCollectionLayoutItem(
+//          layoutSize: NSCollectionLayoutSize(
+//            widthDimension: .fractionalWidth(1/5),
+//            heightDimension: .fractionalHeight(1)
+//          )
+//        )
+//        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 8)
+//
+//        // group
+//        let group = NSCollectionLayoutGroup.horizontal(
+//          layoutSize: NSCollectionLayoutSize(
+//            widthDimension: .fractionalWidth(1),
+//            heightDimension: .absolute(140)
+//          ),
+//          subitem: item,
+//          count: 5
+//        )
+//        group.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 0, bottom: 16, trailing: 0)
+//
+//        // section
+//        let section = NSCollectionLayoutSection(group: group)
+//        section.orthogonalScrollingBehavior = .continuous
+//        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20)
+//
+//        // return
+//        return section
+//
+//      }
+//      return nil
+//    }
+//  }
+//
+//
+//}
